b3eb8e3ef438682820371fdeefde08fbdf5d1543
santiago <santiago.entraigas@alu.ing.unlp.edu.ar>
2022-10-28 14:40:21 -0300

    agrega freeRTOS

---
+++ Debug/Core/Src/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+Core/Src/FreeRTOS/list.o: ../Core/Src/FreeRTOS/list.c \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/projdefs.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/portable.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/list.h
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/projdefs.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/portable.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/list.h:
---
+++ Debug/Core/Src/main.d
@@ -0,0 +1,68 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ ../Core/Inc/stm32f1xx_hal_conf.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ ../Drivers/CMSIS/Include/core_cm3.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/projdefs.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/portable.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/task.h \
+ C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/list.h
+../Core/Inc/main.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+../Core/Inc/stm32f1xx_hal_conf.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+../Drivers/CMSIS/Include/core_cm3.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/projdefs.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/portable.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/task.h:
+C:/Users/Barcala/STM32CubeIDE/workspace_1.10.1/tp9/Core/Inc/FreeRTOS/include/list.h:
